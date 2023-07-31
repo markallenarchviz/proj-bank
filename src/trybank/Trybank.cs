b4c1f71024c117fd0b3121a2733ee88da3ac128e
Mark Allen <markallenarchviz@gmail.com>
2023-07-31 19:50:39 -0300

    Requisito 5

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -82,7 +82,13 @@ public class Trybank
     // 5. Construa a funcionalidade de depositar dinheiro
     public void Deposit(int value)
     {
-        throw new NotImplementedException();
+        if (!Logged)
+            throw new AccessViolationException("Usuário não está logado");
+
+        if (value <= 0)
+            throw new ArgumentException("Valor inválido");
+
+        Bank[loggedUser, 3] += value;
     }
 
     // 6. Construa a funcionalidade de sacar dinheiro
